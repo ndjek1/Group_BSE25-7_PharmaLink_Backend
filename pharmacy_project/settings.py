8111f2d53bb3f8ff78d0a88bc1da67adf407a9f6
ndjek1 <vndjekornom@gmail.com>
2025-09-10 21:02:22 +0300

    Fixing deployment issues

--- pharmacy_project/settings.py
+++ pharmacy_project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z0xaDNFzWQWBqEldFReQJbBmhYgrIi0f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 IS_RENDER = 'RENDER' in os.environ
 
